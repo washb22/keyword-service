51eb9640ed7f3e38d2a34c03ee0af0aa6dbe0269
Jinhyeok Iim <washb1000@gmail.com>
2025-09-05 02:10:35 +0900

    구글로그인

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 # config.py
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    # 아래 SECRET_KEY 라인을 추가하세요.
-    SECRET_KEY = 'super-secret-key' # 실제 서비스에서는 더 복잡하고 긴 값으로 변경해야 합니다.
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SECRET_KEY = 'super-secret-key'  # 실제 서비스에서는 더 복잡하고 긴 값으로 변경해야 합니다.
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
