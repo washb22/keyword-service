be5c2ed66ace20248461bff6baee7362e17ef50d
Jinhyeok Iim <washb1000@gmail.com>
2025-09-05 18:06:05 +0900

    Add automatic database table creation

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-import os
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+# 데이터베이스 테이블 생성 추가
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=False)
